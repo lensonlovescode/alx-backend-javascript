16b217840e66fa0df93814c047b9b02f32c3287e
lensonlovescode <lensonisinparis@gmail.com>
2025-04-24 10:17:04 +0300

    Correctly appended

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  for (let value of array) {
+    array[i] = appendString + value;
+    i++;
+  }
+
+  return array;
+}
