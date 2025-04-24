b8187d5a87e771bc2950d01988cd9173eb64a7f3
lensonlovescode <lensonisinparis@gmail.com>
2025-04-24 10:20:25 +0300

    Correctly fixed

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
-  for (let value of array) {
+  for (const value of array) {
     array[i] = appendString + value;
-    i++;
+    i += 1;
   }
 
   return array;
