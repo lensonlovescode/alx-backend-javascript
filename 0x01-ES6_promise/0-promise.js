59bd40ae6e482b27c24f95af4274df5e019fae7e
lensonlovescode <lensonisinparis@gmail.com>
2025-04-28 12:12:39 +0300

    ESlint corr

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-  if (True) {
-    resolve("Promise type")
-  } else {
-    reject("No promise type")
-  }
-})
+    if (True) {
+      resolve('Promise type');
+    } else {
+      reject('No promise type');
+    }
+  })
 }
