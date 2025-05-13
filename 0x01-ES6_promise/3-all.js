bbe47414826e2a19db4387b0fa84fa55a6a11bce
lensonlovescode <lensonisinparis@gmail.com>
2025-05-13 17:56:26 +0000

    eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,9 @@
-import { uploadPhoto, createUser } from "./utils"
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup()
-{
+export default function handleProfileSignup() {
   return Promise.all([createUser(), uploadPhoto()])
     .then((success) => {
-      console.log(`${success[1].body} ${success[0].firstName} ${success[0].lastName}`)
+      console.log(`${success[1].body} ${success[0].firstName} ${success[0].lastName}`);
     })
-    .catch(() => console.log('Signup system offline'))
+    .catch(() => console.log('Signup system offline'));
 }
