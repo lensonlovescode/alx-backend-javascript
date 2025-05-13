8562b6c688692e6c4e704d535da8b73ee625dd54
lensonlovescode <lensonisinparis@gmail.com>
2025-05-13 20:40:07 +0300

    export default function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from "./utils"
 
-function handleProfileSignup()
+export default function handleProfileSignup()
 {
   return Promise.all([createUser(), uploadPhoto()])
     .then((success) => {
