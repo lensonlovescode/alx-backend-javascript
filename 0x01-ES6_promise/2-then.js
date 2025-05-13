97a302d22db28bac9e3c25bd925f98b034380a43
lensonlovescode <lensonisinparis@gmail.com>
2025-05-13 05:38:22 +0300

    Then what?

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      return ({ status: 200, body: success })
+      console.log('Got a response from the API')
+    });
+    .catch(() => {
+      return (new Error())
+    });
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
