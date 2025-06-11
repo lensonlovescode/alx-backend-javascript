e511760d9a1e4e95cdaca22012f9e04edbf83844
lensonlovescode <lensonisinparis@gmail.com>
2025-06-11 20:48:27 +0300

    After before?

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,39 @@
+function extractSetGroup(arr) {
+  listGroup = [];
+  arr.forEach((line) => {
+    smallArr = line.split(',');
+    listGroup.push(smallArr[smallArr.length - 1]);
+  });
+  setGroup = new Set(listGroup);
+  return setGroup;
+}
+function numberOfStudents(arr, group) {
+  stdArr = [];
+  arr.forEach((item) => {
+    itemArray = item.split(',');
+    if (itemArray[itemArray.length - 1] === group) {
+      stdArr.push(itemArray[0]);
+    }
+  });
+  console.log(`Number of students in ${group}: ${stdArr.length}. List: ${stdArr.join(', ')}`);
+}
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    const fs = require('fs');
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return
+      }
+      const pre_arr = data.split('\n');
+      const arr = pre_arr.filter((line) => line.trim() !== '');
+      console.log(`Number of students: ${arr.length - 1}`);
+      groups = extractSetGroup(arr.slice(1));
+      groups.forEach((group) => {
+        numberOfStudents(arr.slice(1), group);
+      });
+      resolve(true)
+    });
+  });
+}
+module.exports = countStudents;
