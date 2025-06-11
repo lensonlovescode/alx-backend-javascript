054c6bb541e6adfb270f3c5c502d396ae4fd6ea2
lensonlovescode <lensonisinparis@gmail.com>
2025-06-11 21:28:37 +0300

    Simple Http

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const { createServer } = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ALX!');
+});
+
+app.listen(port, hostname);
+module.exports = app;
