a0a379a5b64945740df89a448547faf37abba342
lensonlovescode <lensonisinparis@gmail.com>
2025-06-11 19:31:37 +0300

    Read file sync

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,34 +1,39 @@
-function extractSetGroup(arr) {
-  listGroup = []
+function extractSetGroup (arr) {
+  listGroup = [];
   arr.forEach((line) => {
-    smallArr = line.split(',')
-    listGroup.push(smallArr[smallArr.length - 1])
-  })
-  setGroup = new Set(listGroup)
-  return setGroup
+    smallArr = line.split(',');
+    listGroup.push(smallArr[smallArr.length - 1]);
+  });
+  setGroup = new Set(listGroup);
+  return setGroup;
 }
-function numberOfStudents(arr, Groups) {
-  
-  # prints the students in each group
+function numberOfStudents (arr, group) {
+  stdArr = [];
+  arr.forEach((item) => {
+    itemArray = item.split(',');
+    if (itemArray[itemArray.length - 1] === group) {
+      stdArr.push(itemArray[0]);
+    }
+  });
+  console.log(`Number of students in ${group}: ${stdArr.length}. List: ${stdArr.join(', ')}`);
 }
-function countStudents(path) {
+function countStudents (path) {
   const fs = require('fs');
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const pre_arr = data.split('\n')
-    const arr = pre_arr.filter((line) => line.trim() !== '')
-    console.log(`Number of students: ${arr.length - 1}`)
+    const pre_arr = data.split('\n');
+    const arr = pre_arr.filter((line) => line.trim() !== '');
+    console.log(`Number of students: ${arr.length - 1}`);
 
-    groups = extractSetGroup(arr.slice(1))
+    groups = extractSetGroup(arr.slice(1));
 
     groups.forEach((group) => {
-      numberOfStudents(arr.slice(1), group)
-    })
-    console.log(arr)
+      numberOfStudents(arr.slice(1), group);
+    });
   } catch (err) {
     if (err.code === 'ENOENT') {
       console.log('Cannot load the database');
     }
   }
 }
-module.exports = countStudents
+module.exports = countStudents;
