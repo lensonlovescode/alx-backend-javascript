4f43f34e3793bdb0b79364cf1dd4419dbc3f6c50
lensonlovescode <lensonisinparis@gmail.com>
2025-06-11 12:04:08 +0300

    tbd

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,3 +1,34 @@
-function countStudents (path) {
-
+function extractSetGroup(arr) {
+  listGroup = []
+  arr.forEach((line) => {
+    smallArr = line.split(',')
+    listGroup.push(smallArr[smallArr.length - 1])
+  })
+  setGroup = new Set(listGroup)
+  return setGroup
 }
+function numberOfStudents(arr, Groups) {
+  
+  # prints the students in each group
+}
+function countStudents(path) {
+  const fs = require('fs');
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const pre_arr = data.split('\n')
+    const arr = pre_arr.filter((line) => line.trim() !== '')
+    console.log(`Number of students: ${arr.length - 1}`)
+
+    groups = extractSetGroup(arr.slice(1))
+
+    groups.forEach((group) => {
+      numberOfStudents(arr.slice(1), group)
+    })
+    console.log(arr)
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log('Cannot load the database');
+    }
+  }
+}
+module.exports = countStudents
