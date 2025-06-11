b8f47ed659744593b12eee39e674f33d95fad8f6
lensonlovescode <lensonisinparis@gmail.com>
2025-06-11 10:15:28 +0300

    Jest?

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const Name = process.stdin.read();
   if (Name) {
-    process.stdout.write('Your name is: ' + Name);
+    process.stdout.write(`Your name is: ${Name}`);
   }
 });
 process.stdin.on('end', () => {
