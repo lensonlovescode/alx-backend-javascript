30cc69dcb63817e41f18777074f51129a013604f
lensonlovescode <lensonisinparis@gmail.com>
2025-06-13 23:51:41 +0300

    %lint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
 const { createServer } = require('http');
-const countStudents = require('./3-read_file_async');
+import countStudents from './3-read_file_async.js';
 
 const port = 1245;
 const hostname = '127.0.0.1';
@@ -7,14 +7,14 @@ const hostname = '127.0.0.1';
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  const url = req.url;
+  const { url } = req;
 
   if (url === '/') {
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
     countStudents(process.argv[2])
       .then((msg) => {
-        res.end('This is the list of our students\n' + msg);
+        res.end(`This is the list of our students\n${msg}`);
       })
       .catch(() => {
         res.end('This is the list of our students\nCannot load the database');
