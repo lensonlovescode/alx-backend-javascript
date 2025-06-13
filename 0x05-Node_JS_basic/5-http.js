963eb3edbc92854155f311a34fa350c0276ade47
lensonlovescode <lensonisinparis@gmail.com>
2025-06-13 20:24:40 +0300

    Http 5 corrections

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,15 +7,19 @@ const hostname = '127.0.0.1';
 const app = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
   const url = req.url;
 
   if (url === '/') {
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
-    countStudents(process.argv[2]).then((msg) => {
-      res.end('This is the list of our students\n' + msg);
-    });
+    countStudents(process.argv[2])
+      .then((msg) => {
+        res.end('This is the list of our students\n' + msg);
+      })
+      .catch(() => {
+        res.statusCode = 404;
+        res.end('Cannot load the database')
+      });
   }
 });
 
