40d511c00b94a4e6dff68a5a46b4d39727353a15
lensonlovescode <lensonisinparis@gmail.com>
2025-06-15 10:08:39 +0300

    Eslint 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,16 +2,15 @@ const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const port = 1245;
-const app = express()
-
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 app.get('/students', (req, res) => {
   countStudents(process.argv[2])
     .then((msg) => {
-      res.send('This is the list of our students\n' + msg);
+      res.send(`This is the list of our students\n${msg}`);
     })
     .catch(() => {
       res.send('This is the list of our students\nCannot load the database');
